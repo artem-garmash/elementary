268af4d6abd01b302787170e7e84598ad2b863c2
Artem Garmash <artem.garmash@ouraring.com>
2023-06-12 12:46:51 +0300

    Make alert tables work with Athena

--- elementary/monitor/dbt_project/models/alerts/alerts.sql
+++ elementary/monitor/dbt_project/models/alerts/alerts.sql
@@ -1,8 +1,9 @@
 {{
   config(
     materialized = 'incremental',
+    table_type='iceberg',
+    incremental_strategy='merge',
     unique_key = 'alert_id',
-    merge_update_columns = ['alert_id'],
     on_schema_change = 'append_new_columns'
   )
 }}
@@ -29,8 +30,30 @@ with failed_tests as (
     {% endif %}
 )
 
-select 
-    *,
+select
+    alert_id,
+    data_issue_id,
+    test_execution_id,
+    test_unique_id,
+    model_unique_id,
+    {{ elementary.edr_cast_as_timestamp('detected_at') }} as detected_at,
+    database_name,
+    schema_name,
+    table_name,
+    column_name,
+    alert_type,
+    sub_type,
+    alert_description,
+    owners,
+    tags,
+    alert_results_query,
+    other,
+    test_name,
+    test_short_name,
+    test_params,
+    severity,
+    status,
+    result_rows,
     false as alert_sent,  {# backwards compatibility #}
     'pending' as suppression_status,
     {{ elementary.edr_cast_as_string('NULL') }} as sent_at
--- elementary/monitor/dbt_project/models/alerts/alerts_models.sql
+++ elementary/monitor/dbt_project/models/alerts/alerts_models.sql
@@ -1,8 +1,9 @@
 {{
   config(
     materialized = 'incremental',
+    table_type='iceberg',
+    incremental_strategy='merge',
     unique_key = 'alert_id',
-    merge_update_columns = ['alert_id'],
     on_schema_change = 'append_new_columns'
   )
 }}
@@ -10,8 +11,21 @@
 
 {% set error_models_relation = adapter.get_relation(this.database, this.schema, 'alerts_dbt_models') %}
 {% if error_models_relation %}
-    select 
-      *,
+    select
+      alert_id,
+      unique_id,
+      {{ elementary.edr_cast_as_timestamp('detected_at') }} as detected_at,
+      database_name,
+      materialization,
+      path,
+      original_path,
+      schema_name,
+      message,
+      owners,
+      tags,
+      alias,
+      status,
+      full_refresh,
       false as alert_sent,  {# backwards compatibility #}
       'pending' as suppression_status,
       {{ elementary.edr_cast_as_string('NULL') }} as sent_at
